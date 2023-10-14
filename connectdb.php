7c45f5fcbd88d8d3f8f11ee9b66a171652943db5
LittleCooll <voquochoang2004@gmail.com>
2023-10-14 17:01:17 +0700

    aaa

--- connectdb.php
+++ connectdb.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = new mysqli("localhost","root","", "multilevelnav");
+    $conn = new mysqli("localhost:4306","root","", "websitebacty");
     if ($conn -> connect_error)  {
         die("Ket noi that bai". $conn ->connect_error);
     }
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="style.css">
   <title>Multi-level Navbar</title>
 </head>
 <?php 
@@ -33,6 +33,10 @@
   }
 ?>
 <body>
-    hi
+  <div class="navbar">
+    <?php
+      echo buildMenu($conn); 
+    ?>
+  </div>
 </body>
 </html>
