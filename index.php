71f555149d61d29f4fdd6583df3d60c7e5d6f25b
LittleCooll <voquochoang2004@gmail.com>
2023-10-14 18:23:05 +0700

    123

--- index.php
+++ index.php
@@ -3,26 +3,73 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <style>
+    /* CSS cho menu và submenu */
+    .navbar {
+      font-family: Arial, sans-serif;
+    }
+
+    ul {
+      list-style: none;
+      padding: 0;
+    }
+
+    li {
+      display: inline-block;
+      position: relative;
+      margin-right: 10px;
+    }
+
+    a {
+      text-decoration: none;
+      color: black;
+      padding: 10px;
+      display: block;
+    }
+
+    a:hover {
+      background-color: #f0f0f0;
+    }
+
+    /* Thêm kiểu dáng cho submenu */
+    <?php
+      for ($i = 1; $i <= 10; $i++) {
+        echo ".submenu$i {
+          background-color: #f9f9f9;
+          border: 1px solid #ccc;
+          padding: 10px;
+          position: absolute;
+          display: none;
+          top: 0;
+          left: -150%; /* Hiển thị bên trái */
+        }";
+
+        echo "li:hover .submenu$i {
+          display: block;
+        }";
+      }
+    ?>
+  </style>
   <title>Multi-level Navbar</title>
 </head>
+
 <?php 
   include("connectdb.php");
 
-  function buildMenu($conn, $level = NULL) {
-    $sql = "select * from menu_items";
-    $result = $conn -> query($sql);
+  function buildMenu($conn, $level = NULL, $depth = 1) {
+    $sql = "SELECT * FROM menu_items";
+    $result = $conn->query($sql);
     $menuHTML = "<ul>";
 
-    if ($result -> num_rows > 0) {
-      $menuItems = $result -> fetch_all(MYSQLI_ASSOC);
+    if ($result->num_rows > 0) {
+      $menuItems = $result->fetch_all(MYSQLI_ASSOC);
       foreach ($menuItems as $menuItem) {
         if ($menuItem['level'] == $level) {
-          $menuHTML .= '<li><a href="'. $menuItem['link']. '">'. $menuItem['menu_name']. '</a>';
-
-          $submenu = buildMenu($conn, $menuItem['id']);
+          $submenuClass = "submenu" . $depth; // Tên class submenu với số tầng
+          $menuHTML .= '<li><a href="' . $menuItem['link'] . '">' . $menuItem['menu_name'] . '</a>';
+          $submenu = buildMenu($conn, $menuItem['id'], $depth + 1);
           if (!empty($submenu)) {
-            $menuHTML .= '<div class="submenu">'. $submenu. '</div>';
+            $menuHTML .= '<div class="' . $submenuClass . '">' . $submenu . '</div>';
           }
           $menuHTML .= '</li>';
         }
@@ -32,6 +79,7 @@
     return $menuHTML;
   }
 ?>
+
 <body>
   <div class="navbar">
     <?php
