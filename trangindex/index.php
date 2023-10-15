a70a5f523fd0236c7c7f4a4312631087273a73a3
LittleCooll <voquochoang2004@gmail.com>
2023-10-15 19:55:39 +0700

    aaaa

--- trangindex/index.php
+++ trangindex/index.php
@@ -8,103 +8,12 @@
 </head>
 <body>
     <div class="wrapper">
-        <div class="header">
-        </div>
-        <div class="menu">
-            <ul class="list_menu">
-                <li><a href="index.php">Trang chủ</a></li>
-                <li><a href="#">Danh mục sản phẩm</a></li>
-                <li><a href="#">Giỏ hàng</a></li>
-                <li><a href="#">Liên hệ</a></li>
-            </ul>
-        </div>
-        <div id="main"">
-            <div class="sidebar">
-                <ul class="list_sidebar">
-                    <li><a href="index.php">Trang chủ</a></li>
-                    <li><a href="#">Mô hình</a></li>
-                </ul>
-            </div>
-            <div class="maincontent">
-                <ul class="product_list">
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
-                            <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
-                            <p class="price_product">390.000đ</p>
-                        </a>
-                    </li>
-                    
-                </ul>
-            </div>
-        </div>
-        <div class="clear"></div>
-        <div class="footer">
-            <p class="footer_copyright">Copyright</p>
-        </div>
+        <?php 
+            include("page/header.php");
+            include("page/menu.php");
+            include("page/main.php");
+            include("page/footer.php");   
+        ?>
     </div>
 </body>
 </html>
---
+++ trangindex/page/main.php
@@ -0,0 +1,84 @@
+<div id="main"">
+    <div class="sidebar">
+        <ul class="list_sidebar">
+            <li><a href="index.php">Trang chủ</a></li>
+            <li><a href="#">Mô hình</a></li>
+        </ul>
+    </div>
+    <div class="maincontent">
+        <h4>Sản phẩm mới nhất</h4>
+        <ul class="product_list">
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="images/1898-b014568aa00a1a051b19c2a24087e87e.jpg">
+                    <p class="title_product">Mô hình SPIDER MAN INFINITY WAR</p>
+                    <p class="price_product">390.000đ</p>
+                </a>
+            </li>
+            
+        </ul>
+    </div>
+</div>
